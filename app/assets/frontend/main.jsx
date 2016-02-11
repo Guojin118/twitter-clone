458b7683e82de8a5802bfb2f6ab1c587b0674753
Guojin <guojin.wei@yikotech.com>
2016-02-11 02:02:59 -0800

    Use mockTweets data to show tweets list

---
+++ app/assets/frontend/main.jsx
@@ -0,0 +1,40 @@
+import TweetBox from "./components/TweetBox"
+import TweetList from "./components/TweetList"
+
+let mockTweets = [
+  {id:1, name: 'Guojin', body: 'My #First Tweet'},
+  {id:2, name: 'tt', body: 'My #Second Tweet'},
+  {id:3, name: 'yy', body: 'My #Third Tweet'},
+];
+
+class Main extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {tweetsList: mockTweets }
+  }
+
+  addTweet(tweetToAdd){
+
+  }
+
+  render(){
+    return(
+      <div className="container">
+          <TweetBox sendTweet={this.addTweet.bind(this)} />
+          <TweetList tweets={this.state.tweetsList}/>
+      </div>
+    );
+  }
+}
+
+// var ProductCategoryRow = React.createClass({
+//     render: function() {
+//         return (<tr><th colSpan="2">{this.props.category}</th></tr>);
+//     }
+// });
+
+let documentReady = ( ) => {
+React.render( <Main />, document.getElementById('react') );
+};
+
+$(documentReady);
